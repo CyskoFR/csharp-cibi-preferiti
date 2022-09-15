e0e2f8d241064edf59eda5cf52cdde97b4c179fc
CyskoFR <franc.sko91@gmail.com>
2022-09-15 16:34:14 +0200

    refactoring console.writeline

--- Program.cs
+++ Program.cs
@@ -14,21 +14,17 @@ using System;
 string[] prefFood = { "Mela", "Arancia", "Kiwi", "Pane", "Gelato", "Cioccolato" };
 
 //La lunghezza della classifica
-Console.Write("La lunghezza della classifica: ");
-Console.WriteLine($"{prefFood.Length} elementi in array");
+Console.WriteLine($"La lunghezza della classifica: {prefFood.Length} elementi in array");
 
 //La vostra classifica (dunque stampare l’intero array in ordine indicando la posizione in classifica)
 string[] prefFoodRanking = { prefFood[4], prefFood[1], prefFood[2], prefFood[3], prefFood[0], prefFood[5] };
-Console.Write("La mia classifica: ");
-Console.WriteLine(String.Join(", ", prefFoodRanking));
+Console.WriteLine($"La mia classifica: {String.Join(", ", prefFoodRanking)}");
 
 //Il vostro cibo top (prima posizione della classifica)
-Console.Write("Il mio preferito: ");
-Console.WriteLine(prefFoodRanking[0]);
+Console.WriteLine($"Il mio preferito: {prefFoodRanking[0]}");
 
 //Il vostro cibo preferito ma non troppo (ultima posizione della classifica)
-Console.Write("Il mio preferito ma anche no: ");
-Console.WriteLine(prefFoodRanking[prefFoodRanking.Length-1]);
+Console.WriteLine($"Il mio preferito ma anche no: {prefFoodRanking[prefFoodRanking.Length-1]}");
 
 //BONUS Stampate a video anche il cibo di mezza classifica, cioè che si trova nella posizione mediana. Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari. In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
 var middleFood = "";
@@ -40,6 +36,4 @@ else
 {
     middleFood = ($"{prefFoodRanking[prefFoodRanking.Length/2]}, {prefFoodRanking[prefFoodRanking.Length / 2-1]}");
 }
-
-Console.Write("Il cibo o i cibi di mezza classifica: ");
-Console.WriteLine(middleFood);
+Console.WriteLine($"Il cibo o i cibi di mezza classifica: {prefFoodRanking[prefFoodRanking.Length / 2]}, {prefFoodRanking[prefFoodRanking.Length / 2 - 1]}");
